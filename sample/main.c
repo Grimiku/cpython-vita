2158c94da3b3d4eaa2aead72e5cbe3c8a5f8a8eb
SonicMastr <jaylongowie46@gmail.com>
2022-12-14 19:07:22 -0600

    Finish Sample

--- sample/main.c
+++ sample/main.c
@@ -1,57 +1,30 @@
 #include <stdlib.h>
 #include <Python.h>
 
-int _newlib_heap_size_user = 256 * 1024 * 1024;
+int _newlib_heap_size_user = 100 * 1024 * 1024;
 
 int main(int argc, char *argv[]) {
 	(void)argc;
 	(void)argv;
 
-    //Py_SetPythonHome(L"app0:"); 
-    //Py_SetPath(L"app0:lib/python311.zip;app0:lib/python3.11"); 
+    sceAppMgrUmount("app0:");
 
-    //Py_IsolatedFlag = 1; 
-    Py_VerboseFlag = 0;
-    Py_NoSiteFlag = 1; 
-    //Py_UnbufferedStdioFlag = 1; 
+    // Deprecated
+    Py_SetPythonHome(L"ux0:app/PYTH00003");
 
-    // PyStatus status;
-
-    // PyConfig config; 
-    // PyPreConfig preconfig; 
-    // PyPreConfig_InitIsolatedConfig(&preconfig);
-
-    // preconfig.utf8_mode = 1;
-
-    // status = Py_PreInitialize(&preconfig);
-
-    // if (PyStatus_Exception(status)) {
-    //     Py_ExitStatusException(status);
-    //     sceClibPrintf("Failed\n");
-    // }
-
-    // status = _PyRuntime_Initialize();
-    // if (PyStatus_Exception(status)) {
-    //     Py_ExitStatusException(status);
-    // }
-
-    // PyConfig_InitIsolatedConfig(&config);
-
-    // config.verbose = 0;
-
-    // PyConfig_SetString(&config, &config.run_command, L"main.py");
-
-    // status = Py_InitializeFromConfig(&config); 
-    
-    // PyConfig_Clear(&config);
-    // if (PyStatus_Exception(status)) {
-    //     Py_ExitStatusException(status);
-    // }
+    Py_NoSiteFlag = 1;
+    Py_OptimizeFlag = 2;
 
     Py_Initialize();
-    Py_DebugFlag = 0;
-	PyRun_SimpleString("import __hello__\n");
-    PyRun_SimpleString("__hello__.main()\n");
+	PyRun_SimpleString("import sys\n");
+	PyRun_SimpleString("import os\n");
+	PyRun_SimpleString("print(os.name)\n");
+	PyRun_SimpleString("print(sys.platform)\n");
+	PyRun_SimpleString("print(sys.path)\n");
+	PyRun_SimpleString("print(sys.builtin_module_names)\n");
+	FILE *fp;
+    fp = fopen("ux0:app/PYTH00003/main.py", "r");
+    PyRun_SimpleFile(fp, "main.py");
     Py_Finalize();
 	return 0;
 }
