d0aefc434f0687a01cf47cb161d78acd8f98e86b
Ivan Epifanov <isage.dna@gmail.com>
2022-12-29 11:38:05 +0300

    Allow passing auth token

--- install_extra_modules.sh
+++ install_extra_modules.sh
@@ -10,8 +10,12 @@ fi
 export INSTALLDIR=$PREFIX/lib/python2.7
 
 get_install_latest_release() {
-    echo -e "\033[1;32mInstalling $1/$2\033[0m" 
-    wget -q -O tmp.zip $(curl -s https://api.github.com/repos/$1/$2/releases/latest | grep 'browser_' | cut -d\" -f4) && unzip -o tmp.zip -d $INSTALLDIR && rm tmp.zip
+    echo -e "\033[1;32mInstalling $1/$2\033[0m"
+    if [[ -z "${TOKEN}" ]]; then
+        wget -q -O tmp.zip $(curl -s https://api.github.com/repos/$1/$2/releases/latest | grep 'browser_' | cut -d\" -f4) && unzip -o tmp.zip -d $INSTALLDIR && rm tmp.zip
+    else
+        wget -q -O tmp.zip $(curl -H "Authorization: Bearer ${TOKEN}" -s https://api.github.com/repos/$1/$2/releases/latest | grep 'browser_' | cut -d\" -f4) && unzip -o tmp.zip -d $INSTALLDIR && rm tmp.zip
+    fi
 }
 
 get_install_latest_release SonicMastr youtube-dl
